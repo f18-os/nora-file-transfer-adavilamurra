29b539f61d01cd2c00e8529a2b7e6099f37b6349
Alejandro Davila <adavilamurra@miners.utep.edu>
2018-10-14 14:44:29 -0600

    Sending files at the same time but not getting problems...

--- emphaticDemo/framedThreadClient.py
+++ emphaticDemo/framedThreadClient.py
@@ -2,26 +2,11 @@
 
 # Echo client program
 import socket, sys, re
-import params
 from framedSock import FramedStreamSock
 from threading import Thread
 import time
 
-switchesVarDefaults = (
-    (('-s', '--server'), 'server', "localhost:50001"),
-    (('-d', '--debug'), "debug", False), # boolean (set if present)
-    (('-?', '--usage'), "usage", False), # boolean (set if present)
-    )
-
-
-progname = "framedClient"
-paramMap = params.parseParams(switchesVarDefaults)
-
-server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
-
-if usage:
-    params.usage()
-
+server, usage, debug  = "localhost:50001", False, False
 
 try:
     serverHost, serverPort = re.split(":", server)
@@ -31,26 +16,28 @@ except:
     sys.exit(1)
 
 class ClientThread(Thread):
-    def __init__(self, serverHost, serverPort, debug):
+    def __init__(self, serverHost, serverPort, debug, file_name):
         Thread.__init__(self, daemon=False)
         self.serverHost, self.serverPort, self.debug = serverHost, serverPort, debug
         self.start()
     def run(self):
        s = None
-       for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
+       for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_INET, socket.SOCK_STREAM):
            af, socktype, proto, canonname, sa = res
+           sa = ('127.0.0.1', 50001)
            try:
-               print("creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
-               s = socket.socket(af, socktype, proto)
+               print("Creating sock: af=%d, type=%d, proto=%d" % (af, socktype, proto))
+               s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
            except socket.error as msg:
                print(" error: %s" % msg)
                s = None
                continue
            try:
-               print(" attempting to connect to %s" % repr(sa))
+               print("Attempting to connect to %s" % repr(sa))
                s.connect(sa)
+               print("Connected to server.")
            except socket.error as msg:
-               print(" error: %s" % msg)
+               print("Error: ", msg)
                s.close()
                s = None
                continue
@@ -61,15 +48,21 @@ class ClientThread(Thread):
            sys.exit(1)
 
        fs = FramedStreamSock(s, debug=debug)
+       try:
+           with open(file_name, "r") as clientFile:
+               print("Sending data...")
+               data = clientFile.read()
+               print("Data = ", data[:90])
+               s.send(data.encode())
+               #print("Data sent.")
+               #print("Waiting for data...")
+               #print("received: ", s.recv(100))
+               clientFile.close()
+       except IOError as e:
+           print("No such file or directory.")
+       print("Successfully sent file to server.")
 
-
-       print("sending hello world")
-       fs.sendmsg(b"hello world")
-       print("received:", fs.receivemsg())
-
-       fs.sendmsg(b"hello world")
-       print("received:", fs.receivemsg())
-
-for i in range(100):
-    ClientThread(serverHost, serverPort, debug)
-
+print("--- File Transfer ---")
+file_name = input("Enter name of file to send: ")
+for i in range(5):
+    ClientThread(serverHost, serverPort, debug, file_name)
